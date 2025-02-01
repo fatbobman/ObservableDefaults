a49c15520fb56bfebe2ed2203ff515236088c217
Joe Neuman <finder39@gmail.com>
2025-02-01 11:11:05 -0800

    chore: update swift-syntax location to swiftlang/swift-syntax

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
