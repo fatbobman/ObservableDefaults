d08d7aaee374a39ac8f1a2812213299ba3f4cf84
zangqilong <zangqilong@gmail.com>
2025-05-26 18:59:52 +0800

    feat: Update swift-syntax version
    Signed-off-by: zangqilong <zangqilong@gmail.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
